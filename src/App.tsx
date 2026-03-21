cf1fce5d48fe8b05cf4dfb6dd535083004075ff1
kunjshah1908 <kunjshah2112@gmail.com>
2026-03-22 01:59:37 +0530

    command center

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Dashboard from './pages/Dashboard';
 import RegionalOfficerDashboard from './pages/RegionalOfficerDashboard';
 import RegionalOfficerAuth from './pages/RegionalOfficerAuth';
 import RequireRegionalAuth from './components/RequireRegionalAuth';
+import CommandCenterAuth from './pages/CommandCenterAuth';
+import RequireCommandCenterAuth from './components/RequireCommandCenterAuth';
 import History from './pages/History';
 import './styles.css';
 
@@ -11,7 +13,15 @@ const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Landing />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/dashboard" element={<CommandCenterAuth />} />
+      <Route
+        path="/dashboard/home"
+        element={
+          <RequireCommandCenterAuth>
+            <Dashboard />
+          </RequireCommandCenterAuth>
+        }
+      />
       <Route path="/regional" element={<RegionalOfficerAuth />} />
       <Route
         path="/regional/dashboard"
